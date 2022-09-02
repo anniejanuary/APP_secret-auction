6b8d5bc2ffee2564fe58abadb295b3ac17913d0a
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-09-02 11:47:43 +0200

    Update APP_secret-auction.py

--- APP_secret-auction.py
+++ APP_secret-auction.py
@@ -1,23 +1,40 @@
 from replit import clear
-#HINT: You can call clear() to clear the output in the console.
 
 from art import logo
 print(logo)
 
+bid_log = {}
+
 print("Welcome to the Secret Auction!")
 
-bid_log = []
-
 keep_going = True
+
+def winner (bidding_record): ##  made up "bidding_record" dictionary placeholder just now that will later take bid_log dictionary as input to sift through it
+    highest_bid = 0
+    winner_name = ""
+
+    for person in bidding_record:
+      bidding_amount = bidding_record[person]
+      if bidding_amount > highest_bid:
+        highest_bid = bidding_amount 
+        winner_name = person
+    
+    print(f"{winner_name} won the Silent Auction with a bid of ${highest_bid}!")
+
+# finding the max bid value in a dictionary of several key-value pairs:
+def add_new_bid (new_name, new_bid):    
+  ## this creates: {"Kara" : 200}
+  bid_log[name] = bid
+    
+  ## this would create : {"name" : Kara, "bid" : 200}
+  # bid_log["name"] = new_name
+  # bid_log["bid"] = new_bid
+
 while keep_going:
   name = input("What's your name: ")
   bid = int(input("Enter your bid: $"))
 
-  def add_new_bid (new_name, new_bid):
-    new_bid_dictionary = {}
-    new_bid_dictionary["name"] = new_name
-    new_bid_dictionary["bid"] = new_bid
-    bid_log.append(new_bid_dictionary)
+  # finding the max bid value in a dictionary of several key-value pairs:  
   add_new_bid (name, bid)
 
   again = input("Would you like to add another bidder? yes/no: ")
@@ -25,12 +42,33 @@ while keep_going:
     clear()
   else:
     keep_going = False
-
-    # finding the max bid value in a list of dictionaries
-    max_bid = max(bid_log, key=lambda x:x['bid'])
+    winner (bid_log)
     
-    #print(bid_log)
-    print(f"{max_bid['name']} won the Silent Auction with a bid of ${max_bid['bid']}!")
+    
+### way 2: adding new bids as separate dictionaries inside a list
+# bid_log = []
 
+# keep_going = True
+# while keep_going:
+#   name = input("What's your name: ")
+#   bid = int(input("Enter your bid: $"))
+
+#   def add_new_bid (new_name, new_bid):
+#     new_bid_dictionary = {}
+#     new_bid_dictionary["name"] = new_name
+#     new_bid_dictionary["bid"] = new_bid
+#     bid_log.append(new_bid_dictionary)
+#   add_new_bid (name, bid)
+
+#   again = input("Would you like to add another bidder? yes/no: ")
+#   if again == "yes":
+#     clear()
+#   else:
+#     keep_going = False
+
+#     # LAMBDA: finding the max bid value in a list of dictionaries
+#     max_bid = max(bid_log, key=lambda x:x['bid'])
+#     #print(bid_log)
+#     print(f"{max_bid['name']} won the Silent Auction with a bid of ${max_bid['bid']}!")
 
 
